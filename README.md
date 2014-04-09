95017647769dbfdfe9ee91f45babab0e4d379394
Victor Venters <victor@huge-success.net>
2014-04-09 04:18:01 -0700

    Basic Setup

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ famous
 ======
 
 Famous Library packaged for Meteor.
+
+Coming Soon
+---------------------
+
+This is a placeholder for the new client side Famous framework. Once the souce code is relased I will update this package.
---
+++ package.js
@@ -0,0 +1,10 @@
+{           
+  "name": "Famous",       
+  "description": "Famous packaged for Meteor",
+  "homepage": "https://github.com/hugesuccess/famous",
+  "author": "Huge Success (http://hugesuccess.co)",
+  "version": "0.0.1",        
+  "git": "https://github.com/hugesuccess/famous.git",
+  "packages": { }
+}       
+~ 
